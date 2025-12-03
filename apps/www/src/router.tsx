738ae2447af6813d0a120ad1ea390153eb7f649d
Altanbagana Erdene-Ochir <altanbagana@protonmail.com>
2025-12-03 18:48:37 +0800

    chore: trying hash routing

--- apps/www/src/router.tsx
+++ apps/www/src/router.tsx
@@ -1,10 +1,13 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router';
+import { createHashHistory, createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { NotFound } from '@/src/components/not-found';
+
+const hashHistory = createHashHistory();
 
 export function getRouter() {
   return createTanStackRouter({
     routeTree,
+    history: hashHistory,
     defaultPreload: 'intent',
     scrollRestoration: true,
     defaultNotFoundComponent: NotFound,
