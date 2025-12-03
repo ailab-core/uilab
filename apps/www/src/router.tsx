96901579aa58df2aadf87112c99ced679bd78c3e
Altanbagana Erdene-Ochir <altanbagana@protonmail.com>
2025-12-03 18:49:21 +0800

    ci: reverting hash routing, apparently ssl fixed it

--- apps/www/src/router.tsx
+++ apps/www/src/router.tsx
@@ -1,13 +1,10 @@
-import { createHashHistory, createRouter as createTanStackRouter } from '@tanstack/react-router';
+import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import { NotFound } from '@/src/components/not-found';
-
-const hashHistory = createHashHistory();
 
 export function getRouter() {
   return createTanStackRouter({
     routeTree,
-    history: hashHistory,
     defaultPreload: 'intent',
     scrollRestoration: true,
     defaultNotFoundComponent: NotFound,
