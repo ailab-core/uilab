ec2a4cde37665237d5da8b579c2ffc0ec0bc12d5
Altanbagana Erdene-Ochir <altanbagana@protonmail.com>
2026-02-17 10:58:20 +0800

    fix(responsive): landing page

--- apps/www/lib/layout.shared.tsx
+++ apps/www/lib/layout.shared.tsx
@@ -1,9 +1,18 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { BookOpenTextIcon } from 'lucide-react';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: 'UILab',
     },
+    githubUrl: 'https://github.com/ailab-core/uilab',
+    links: [
+      {
+        text: 'Documentation',
+        url: '/docs',
+        icon: <BookOpenTextIcon />
+      },
+    ]
   };
 }
